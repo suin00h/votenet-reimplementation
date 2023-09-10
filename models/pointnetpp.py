41becf0e5e84ee875c4b65da3d18746e24e727cf
suin00h <suin00h@korea.ac.kr>
2023-09-10 17:45:28 +0900

    refactor: moved pointnetpp submodules

--- models/pointnetpp.py
+++ models/pointnetpp.py
@@ -1,13 +1,10 @@
-import torch
 import torch.nn as nn
 from torch import Tensor
-from torch.autograd import Function
 
 from pointnetpp_utils import (
-    farthestPointSampling,
-    gatherPoints,
-    gatherPointsGrad,
-    ballQuery
+    FarthestPointSampling,
+    GatherPoints,
+    GroupingLayer
 )
 
 class PointNetpp(nn.Module):
@@ -37,12 +34,10 @@ class SetAbstractionLayer(nn.Module):
         super().__init__()
         
         self.num_sample = num_sample
-        self.radius = radius
-        self.max_num_cluster = max_num_cluster
         
         self.sampling = FarthestPointSampling.apply
         self.gather = GatherPoints.apply
-        self.grouping = BallQuery.apply
+        self.grouping = GroupingLayer(radius, max_num_cluster)
 
     def forward(
         self,
@@ -68,96 +63,13 @@ class SetAbstractionLayer(nn.Module):
         centroid_indices = self.sampling(point_coord, self.num_sample)
         centroid_coord = self.gather(point_coord, centroid_indices)
         
-        grouped_points = self.grouping(point_coord, centroid_coord, self.radius, self.max_num_cluster)
+        grouped_features = self.grouping(point_coord, centroid_coord, features)
         
-        return grouped_points
-
-class FarthestPointSampling(Function):
-    """
-    Custom function implementation of farthest point sampling(FPS).
-    Wrapping farthestPointSampling function.
-    
-    Args:
-        point_coord: (B, N, 3) tensor
-        num_sample: number of sampled points
-    Returns:
-        sample_indices: (B, num_sample) tensor of indices
-    """
-    @staticmethod
-    def forward(
-        ctx,
-        point_coord: Tensor,
-        num_sample: int
-    ) -> Tensor:
-        sample_indices = farthestPointSampling(point_coord, num_sample)
-        ctx.mark_non_differentiable(sample_indices)
-        
-        return sample_indices
-    
-    @staticmethod
-    def backward(ctx, grad_output=None):
-        return None, None
-
-class GatherPoints(Function):
-    """
-    Custom function for slicing point cloud tensors according to given indices.
-    Wrapping gatherPoints and gatherPointsGrad function.
-    
-    Args:
-        points: (B, N, 3) tensor
-        indices: (B, N') tensor containing target indices
-    Returns:
-        new_points: (B, N', 3) tensor
-    """
-    @staticmethod
-    def forward(
-        ctx,
-        points: Tensor,
-        indices: Tensor
-    ):
-        ctx.for_backwards = (indices, points.shape)
-        
-        new_points = gatherPoints(points, indices)
-        return new_points
-    
-    @staticmethod
-    def backward(ctx, grad_output):
-        indices, shape = ctx.for_backwards
-        
-        grad_features = gatherPointsGrad(grad_output, indices, shape)
-        return grad_features, None
-
-class BallQuery(Function):
-    """
-    Custom function for ball query algorithm implementation.
-    Wrapping ballQuery function.
-    
-    Args:
-        point_coord: (B, N, 3) tensor
-        centroid_coord: (B, N', 3) tensor
-        radius: radius of the ball
-        max_num_cluster: maximum number of points in the ball
-    Returns:
-        cluster_point_indices: (B, N', num_sample) tensor containing indicies of each cluster
-    """
-    @staticmethod
-    def forward(
-        ctx,
-        point_coord: Tensor,
-        centroid_coord: Tensor,
-        radius: float,
-        max_num_cluster: int
-    ):
-        cluster_point_indices = ballQuery(point_coord, centroid_coord, radius, max_num_cluster)
-        ctx.mark_non_differentiable(cluster_point_indices)
-        
-        return cluster_point_indices
-    
-    @staticmethod
-    def backward(ctx, grad_output):
-        return None, None, None, None
+        return grouped_features
 
 if __name__ == "__main__":
+    import torch
+    
     sample_input = torch.randn((10, 100, 3), requires_grad=True)
     sa = SetAbstractionLayer(10, 0.2, 5)
     output = sa(sample_input, None)
--- models/pointnetpp_utils.py
+++ models/pointnetpp_utils.py
@@ -2,7 +2,34 @@ import random
 from typing import Sequence
 
 import torch
+from torch import nn
 from torch import Tensor
+from torch.autograd import Function
+
+class FarthestPointSampling(Function):
+    """
+    Custom function implementation of farthest point sampling(FPS).
+    Wrapping farthestPointSampling function.
+    
+    Args:
+        point_coord: (B, N, 3) tensor
+        num_sample: number of points to be sampled
+    Returns:
+        sample_idxs: (B, num_sample) tensor of indices
+    """
+    @staticmethod
+    def forward(
+        ctx,
+        point_coord: Tensor,
+        num_sample: int
+    ) -> Tensor:
+        sample_idxs = farthestPointSampling(point_coord, num_sample)
+        ctx.mark_non_differentiable(sample_idxs)
+        return sample_idxs
+    
+    @staticmethod
+    def backward(ctx, grad_output=None):
+        return None, None
 
 def farthestPointSampling(
     point_coord: Tensor,
@@ -14,78 +41,139 @@ def farthestPointSampling(
     
     Args:
         point_coord: (B, N, 3) tensor
-        num_sample: number of sampled points
+        num_sample: number of points to be sampled
     Returns:
-        sample_indices: (B, num_sample) tensor of indices
+        sample_idxs: (B, num_sample) tensor of indices
     """
-    num_batch, num_points, _ = point_coord.size()
+    num_batch, num_points, _ = point_coord.shape
     device = point_coord.device
     
-    sample_indices = []    
-    for batch in range(num_batch):
+    sample_idxs = []    
+    for b in range(num_batch):
         sample_idx = torch.full((num_sample, ), -1, dtype=torch.int64, device=device)
-        closest_distances = point_coord.new_full((num_points, ), float("inf"), dtype=torch.float32)
-        selected_idx = random.randint(0, num_points)
-        sample_idx[0] = selected_idx
+        closest_dist = torch.full((num_points, ), float("inf"), dtype=torch.float32, device=device)
+        selected_idx = random.randint(0, num_points - 1)
         
+        sample_idx[0] = selected_idx
         for i in range(num_sample):
-            distance = point_coord[batch, selected_idx, :] - point_coord[batch, :num_points, :]
-            distance = (distance**2).sum(-1)
+            dist = point_coord[b, selected_idx, :] - point_coord[b, :num_points, :]
+            dist = (dist ** 2).sum(-1)
             
-            closest_distances = torch.min(distance, closest_distances)
+            closest_dist = torch.min(dist, closest_dist)
             
-            selected_idx = torch.argmax(closest_distances)
+            selected_idx = torch.argmax(closest_dist)
             sample_idx[i] = selected_idx
         
-        sample_indices.append(sample_idx)
-    sample_indices = torch.stack(sample_indices)
+        sample_idxs.append(sample_idx)
     
-    return sample_indices
+    sample_idxs = torch.stack(sample_idxs)
+    return sample_idxs
+
+class GatherPoints(Function):
+    """
+    Custom function for slicing point cloud tensors according to given indices.
+    Wrapping gatherPoints and gatherPointsGrad function.
+    
+    Args:
+        points: (B, N, C) tensor
+        idxs: (B, N') tensor containing target indices
+    Returns:
+        new_points: (B, N', C) tensor
+    """
+    @staticmethod
+    def forward(
+        ctx,
+        points: Tensor,
+        idxs: Tensor
+    ):
+        ctx.for_backwards = (idxs, points.size(1))
+        
+        new_points = gatherPoints(points, idxs)
+        return new_points
+    
+    @staticmethod
+    def backward(ctx, grad_output):
+        idxs, num_points = ctx.for_backwards
+        
+        grad_input = gatherPointsGrad(grad_output, idxs, num_points)
+        return grad_input, None
 
 def gatherPoints(
     points: Tensor,
-    indices: Tensor
+    idxs: Tensor
 ):
     """
     Slicing point cloud tensors according to given indices.
     
     Args:
         points: (B, N, C) tensor
-        indices: (B, N') tensor containing target indices
+        idxs: (B, N') tensor containing target indices
     Returns:
         new_points: (B, N', C) tensor
     """
-    new_points_list = []
-    for b, indices_batch in enumerate(indices):
-        new_points_list_batch = [points[b, index, :] for index in indices_batch]
-        new_points_batch = torch.stack(new_points_list_batch)
-        
-        new_points_list.append(new_points_batch)
+    new_points = torch.zeros(
+        (points.size(0), idxs.size(1), points.size(2)),
+        dtype=torch.float32, device=points.device
+    )
     
-    new_points = torch.stack(new_points_list)
+    for b in range(points.size(0)):
+        for i, idx in enumerate(idxs[b]):
+            new_points[b, i] = points[b, idx]
     return new_points
 
 def gatherPointsGrad(
     grad_output: Tensor,
-    indices: Tensor,
-    shape: Sequence[int]
+    idxs: Tensor,
+    num_points: int
 ):
     """
     Args:
         grad_output: (B, N', C) tensor of upstream gradients
-        indices: (B, N') tensor containg target indices for forward calculation
-        shape: (3) sequnece containing original tensor size components
+        idxs: (B, N') tensor containg target indices for forward calculation
+        num_points: number of original points
     Returns:
-        grad_features: (B, N, C) tensor of reconstructed downstream gradients
+        grad_input: (B, N, C) tensor of reconstructed downstream gradients
     """
-    num_batch = shape[0]
-    grad_features = torch.zeros(shape)
+    grad_input = torch.zeros(
+        (grad_output.size(0), num_points, grad_output.size(2)),
+        dtype=torch.float32, device=grad_output.device
+    )
     
-    for b in range(num_batch):
-        for i, index in enumerate(indices[b]):
-            grad_features[b, index] = grad_output[b, i]
+    for b in range(grad_output.size(0)):
+        for i, idx in enumerate(idxs[b]):
+            grad_input[b, idx] += grad_output[b, i]
+    return grad_input
+
+class BallQuery(Function):
+    """
+    Custom function implementation of ball query algorithm.
+    Wrapping ballQuery function.
     
-    return grad_features
+    Args:
+        point_coord: (B, N, 3) tensor
+        centroid_coord: (B, N', 3) tensor
+        radius: radius of the ball
+        max_num_cluster: maximum number of points in the ball
+    Returns:
+        cluster_point_indices: (B, N', num_sample) tensor containing indicies of each cluster
+    """
+    @staticmethod
+    def forward(
+        ctx,
+        point_coord: Tensor,
+        centroid_coord: Tensor,
+        radius: float,
+        max_num_cluster: int
+    ):
+        cluster_point_indices = ballQuery(point_coord, centroid_coord, radius, max_num_cluster)
+        ctx.mark_non_differentiable(cluster_point_indices)
+        
+        return cluster_point_indices
+    
+    @staticmethod
+    def backward(ctx, grad_output):
+        return None, None, None, None
+
 
 def ballQuery(
     point_coord: Tensor,
@@ -134,3 +222,70 @@ def ballQuery(
     
     cluster_point_indices = torch.stack(cluster_point_indices_list)
     return cluster_point_indices
+
+class GroupingLayer(nn.Module):
+    """
+    Grouping point features with a ball query.
+    
+    Args:
+        radius: radius of the ball
+        max_num_cluster: maximum number of points in the ball
+        use_coord: whether to use coordinate as a feature
+        normalize_coord: whether to normalize coordinates
+    """
+    def __init__(
+        self,
+        radius: float,
+        max_num_cluster: int,
+        use_coord: bool=True,
+        normalize_coord: bool=True
+    ):
+        super().__init__()
+        
+        self.radius = radius
+        self.max_num_cluster = max_num_cluster
+        self.use_coord = use_coord
+        self.normalize_coord = normalize_coord
+        
+        self.ball_query = BallQuery.apply
+        self.grouper = ...
+    
+    def forward(
+        self,
+        point_coord: Tensor,
+        centroid_coord: Tensor,
+        features: Tensor=None
+    ):
+        """
+        Args:
+            point_coord: (B, N, 3) tensor
+            centroid_coord: (B, N', 3) tensor
+            features: (B, C, N) tensor
+        
+        Returns:
+            grouped_features: (B, C+3, N', K)
+        """
+        cluster_indices = self.ball_query(point_coord, centroid_coord, self.radius, self.max_num_cluster)
+        
+        grouped_coord = grouped_coord.transpose(1, 2).contiguous()
+        grouped_coord = self.grouper(point_coord, cluster_indices)
+        grouped_coord -= centroid_coord.transpose(1, 2).unsqueeze(-1)
+        if self.normalize_coord:
+            grouped_coord /= self.radius
+            
+        if features is None:
+            return grouped_coord
+        grouped_features = self.grouper(features, cluster_indices)
+        if self.use_coord:
+            grouped_features = torch.cat((grouped_coord, grouped_features), dim=1)
+            
+        return grouped_features
+
+if __name__ == "__main__":
+    from torch.autograd import gradcheck
+    B, N, C = (2, 3, 2)
+    x = torch.randn((B, N, 2), dtype=torch.float32, requires_grad=True)
+    y = FarthestPointSampling.apply(x, 2)
+    print(x, y)
+    print(gradcheck(GatherPoints.apply, (x, y)))
+    # print(gradcheck(FarthestPointSampling.apply, (x, 3)))
